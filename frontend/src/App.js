88b1f7ac38d7672265664b19c4c39652ffcfb735
Iskender Akhmedov <ia913@nyu.edu>
2019-10-14 18:44:45 -0400

    Closes #28, sets up basic MERN stack, connects backend to frontend via axios, sets up basic endpoints, connects to MongoDB

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css';
+
+import Navbar from "./components/Navbar.js"
+import CreateUser from "./components/CreateUser.js"
+
+function App() {
+  return (
+      <Router>
+        <Navbar />
+        <br/>
+        <Route path="/user" component={CreateUser} />
+      </Router>
+  );
+}
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,13 +1,28 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 8080;
-const path = require('path');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
 const app = express();
-app.use(bodyParser.json());
-app.use(express.static(`${__dirname}/../react-client/dist`));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(`${__dirname}/../react-client/dist/index.html`));
-});
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}!`);
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+const usersRouter = require('./routes/users');
+
+app.use('/users', usersRouter);
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
 });
