7d2954afb7b184293891ae25f3746672de5290e8
Anastasia <54910828+anastasiasoenjoto@users.noreply.github.com>
2019-10-21 00:24:28 -0400

    Create unit test for CreateUser.js. Updated dependencies to support jest and enzyme

--- frontend/src/components/CreateUser.js
+++ frontend/src/components/CreateUser.js
@@ -113,37 +113,37 @@ export default class CreateUser extends Component {
     return (
       <div>
         <h1> Sign Up </h1>
-        <form onSubmit={this.onSubmit} >
+        <form id="signUpForm" onSubmit={this.onSubmit} >
           <fieldset>
-          <legend class="formHeader"> Sign up Information</legend>
+          <legend className="formHeader"> Sign up Information</legend>
           <label>
             First Name: 
-            <input type="text" value={this.state.firstName} onChange= {this.onChangefirstName} placeholder="Enter first name"/>
+            <input id="firstName" type="text" value={this.state.firstName} onChange= {this.onChangefirstName} placeholder="Enter first name"/>
           </label>
           <br></br>
             <label>
             Last Name: 
-            <input type="text" value={this.state.lastName} onChange= {this.onChangelastName} placeholder="Enter last name"/>
+            <input id="lastName" type="text" value={this.state.lastName} onChange= {this.onChangelastName} placeholder="Enter last name"/>
           </label>
           <br></br>
           <label>
             UserName: 
-            <input type="text" value={this.state.username} onChange= {this.onChangeUsername} placeholder="Enter last name"/>
+            <input id="userName" type="text" value={this.state.username} onChange= {this.onChangeUsername} placeholder="Enter last name"/>
           </label>
           <br></br>
           <label>
             E-mail: 
-            <input type="email" value={this.state.email} onChange= {this.onChangeEmail} placeholder="Enter e-mail"/>
+            <input id="email" type="email" value={this.state.email} onChange= {this.onChangeEmail} placeholder="Enter e-mail"/>
           </label>
           <br></br>
           <label>
             Password: 
-            <input type="password" value={this.state.password} onChange= {this.onChangePassword} placeholder="Enter password"/>
+            <input id="password" type="password" value={this.state.password} onChange= {this.onChangePassword} placeholder="Enter password"/>
           </label>
           <br></br>
           <label>
             City: 
-            <select value={this.state.city} onChange= {this.onChangeCity}>
+            <select id="city" value={this.state.city} onChange= {this.onChangeCity}>
               <option>----Select a city-----</option>
               <optgroup label="California">
                 <option value="losAngeles">Los Angeles</option>
@@ -163,10 +163,10 @@ export default class CreateUser extends Component {
           </fieldset>
           <br></br>
           <fieldset>
-            <legend class="formHeader"> Interest </legend>
+            <legend className="formHeader"> Interest </legend>
             <label>
             Major: 
-            <select value={this.state.major} onChange= {this.onChangeMajor}>
+            <select id="major" value={this.state.major} onChange= {this.onChangeMajor}>
               <option>----Select your major-----</option>
               
               <option value="anthropology">Anthropology</option>
@@ -179,7 +179,7 @@ export default class CreateUser extends Component {
           </fieldset>
           <label>
             GPA: 
-            <input type="text" value={this.state.GPA} onChange= {this.onChangeGPA} placeholder="Enter GPA"/>
+            <input id="GPA" type="text" value={this.state.GPA} onChange= {this.onChangeGPA} placeholder="Enter GPA"/>
           </label>
           <br></br>
           <input type="submit"></input>
