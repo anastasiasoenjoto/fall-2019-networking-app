fbe9bd6db65085aed3624687ffafe8c2db21d306
Anastasia <54910828+anastasiasoenjoto@users.noreply.github.com>
2019-10-21 00:43:29 -0400

    removed comments in navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -21,23 +21,6 @@ export default class Navbar extends Component {
         </ul>
         </div>
       </nav>
-      // <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-      //   <Link to="/" className="navbar-brand">Network</Link>
-      //   <div className="collpase navbar-collapse">
-      //   <ul className="navbar-nav mr-auto">
-      //     <li className="navbar-item">
-      //     <Link to="/user" className="nav-link">Create User</Link>
-      //     </li>
-      //     <li className="navbar-item"> 
-      //     <Link to="/activeUsers" className="nav-link">Display Users</Link>
-
-      //     <li className="navbar-item">
-      //     <Link to="/company" className="nav-link">Create Company User</Link>
-
-      //     </li>
-      //   </ul>
-      //   </div>
-      // </nav>
     );
   }
 }
