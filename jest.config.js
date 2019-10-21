80471e056a9c80f52678bd76c4e61245cc0a695a
Anastasia <54910828+anastasiasoenjoto@users.noreply.github.com>
2019-10-21 00:24:28 -0400

    Create unit test for CreateUser.js. Updated dependencies to support jest and enzyme

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    collectCoverage: true,
+    setupFiles: ['<rootDir>/test/setup.js'],
+    transform: {
+        "^.+\\.js$": "<rootDir>/jest-transformer.js"
+    }, 
+    transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$"]
+    
+    
+}
