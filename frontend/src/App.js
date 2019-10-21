8ab9e1c90a63a6be72a9855ec49226dd70d4ede7
Anastasia <54910828+anastasiasoenjoto@users.noreply.github.com>
2019-10-21 00:42:46 -0400

    Fixed issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,11 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
 
 import Navbar from "./components/Navbar.js"
-<<<<<<< HEAD
 import UsersList from "./components/Signup.js"
-=======
+
 import CreateUser from "./components/CreateUser.js"
 import CreateCompany from "./components/CreateCompany"
->>>>>>> 09f7ee41ab150fe5f9dcc8449cba3e02728b16dc
+
 
 function App() {
   return (
@@ -17,11 +16,10 @@ function App() {
         <Navbar />
         <br/>
         <Route path="/user" component={CreateUser} />
-<<<<<<< HEAD
-
-=======
         <Route path="/company" component={CreateCompany} />
->>>>>>> 09f7ee41ab150fe5f9dcc8449cba3e02728b16dc
+        <Route path="/activeUsers" component={UsersList} />
+
+
       </Router>
   );
 }
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,24 +5,39 @@ export default class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">Network</Link>
-        <div className="collpase navbar-collapse">
-        <ul className="navbar-nav mr-auto">
-          <li className="navbar-item">
-          <Link to="/user" className="nav-link">Create User</Link>
-          </li>
-<<<<<<< HEAD
-          <li className="navbar-item"> 
-          <Link to="/activeUsers" className="nav-link">Display Users</Link>
-=======
+        <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link to="/" className="navbar-brand">Network</Link>
+        <div className="collpase navbar-collapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
+          <Link to="/user" className="nav-link">Create User</Link>
+          </li>
+          <li className="navbar-item"> 
+          <Link to="/activeUsers" className="nav-link">Display Users</Link>
+          </li>
           <li className="navbar-item">
           <Link to="/company" className="nav-link">Create Company User</Link>
->>>>>>> 09f7ee41ab150fe5f9dcc8449cba3e02728b16dc
           </li>
-        </ul>
-        </div>
-      </nav>
+        </ul>
+        </div>
+      </nav>
+      // <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+      //   <Link to="/" className="navbar-brand">Network</Link>
+      //   <div className="collpase navbar-collapse">
+      //   <ul className="navbar-nav mr-auto">
+      //     <li className="navbar-item">
+      //     <Link to="/user" className="nav-link">Create User</Link>
+      //     </li>
+      //     <li className="navbar-item"> 
+      //     <Link to="/activeUsers" className="nav-link">Display Users</Link>
+
+      //     <li className="navbar-item">
+      //     <Link to="/company" className="nav-link">Create Company User</Link>
+
+      //     </li>
+      //   </ul>
+      //   </div>
+      // </nav>
     );
   }
 }
