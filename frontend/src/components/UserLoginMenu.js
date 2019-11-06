c61d561f9f81dbe10a32e330e923172af98606d4
Frgrg123 <williamjiang0505@hotmail.com>
2019-11-06 06:49:44 -0500

    started working on login menu

---
+++ frontend/src/components/UserLoginMenu.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
