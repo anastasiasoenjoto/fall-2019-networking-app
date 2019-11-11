22b51bc68c833ab5d5c76511aaa2115999ac71a4
Anastasia <54910828+anastasiasoenjoto@users.noreply.github.com>
2019-11-11 12:47:29 -0500

    login page

--- frontend/src/components/LoginUser.js
+++ frontend/src/components/LoginUser.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Redirect } from 'react-router-dom'
+import Button from '@material-ui/core/Button'
 
 
 export default class LoginUser extends Component {
@@ -38,9 +40,14 @@ export default class LoginUser extends Component {
       password: this.state.password
     }
 
-
-    axios.post('', user)
+    try {
+      axios.get('http://localhost:3001/users/validateUser', user)
       .then(res => console.log(res.data));
+
+    } catch (error) {
+      console.log(error)
+    }
+    
 
     this.setState({
       username: '',
@@ -49,25 +56,32 @@ export default class LoginUser extends Component {
 
   }
 
+ 
+
   render() {
+    const lableStyle = {
+      margin: '10px', 
+      textAlign: 'center'
+    };
+
     return (
       <div>
-        <h1> Login </h1>
         <form id="signUpForm" onSubmit={this.onSubmit} >
           <fieldset>
           <legend className="formHeader"> Login</legend>
-          <label>
+          <label style={lableStyle}>
             Username:  
             <input id="userName" type="text" value={this.state.username} onChange= {this.onChangeUsername} placeholder="Enter username"/>
           </label>
           <br></br>
-            <label>
+            <label style={lableStyle}>
             Password: 
             <input id="password" type="password" value={this.state.password} onChange= {this.onChangePassword} placeholder="Enter password"/>
           </label>
           </fieldset>
           <br></br>
           <input type="submit"></input>
+        {/* <Button variant="contained" color="primary"> Reset</Button> */}
         </form>
         Dont have an account? Click <a href="/user">here </a> to signup!
 
