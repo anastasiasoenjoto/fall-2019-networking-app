370b6f8508329658e4c526cd398758edd2d96b16
William Jiang <wj537@nyu.edu>
2019-12-13 20:38:08 -0500

    bug fixes from merges

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -70,7 +70,7 @@ class HomePage extends Component {
             redirectToApply: false , 
             jobId: '', 
             currentPending: [], 
-            currentFriends: []
+            currentFriends: [],
             jobsApplied: 0
         };
 
